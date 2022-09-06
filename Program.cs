419883c8f9d7635589fcc2aaf035b5278a3c7b14
Daria <dashamelnik293@gmail.com>
2022-09-06 19:29:40 +0300

    first task

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿try
+{
+Console.WriteLine ("Введите трёхзначное число");
+int a = Convert.ToInt32 (Console.ReadLine ());
+if (a > 999) 
+{
+Console.WriteLine ("Нужно ввести трёхзначное число!");
+}
+else if (a < 100) 
+{
+Console.WriteLine ("Нужно ввести трёхзначное число!");
+}
+else 
+{
+int a2 = ((a % 100) - (a % 10)) / 10;
+Console.WriteLine ("Вторая цифра " + a2);
+};
+}
+catch 
+{
+Console.WriteLine ("Нужно ввести трёхзначное число!");
+};
+
